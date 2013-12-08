0805620b4814c89c569625eb6b35d56e4c7ed0fe
timnp <tinpe@gmx.de>
2013-12-08 16:22:29 +0100

    added function assign, which evaluates a Formula

--- datatypes/Formula.java
+++ datatypes/Formula.java
@@ -1,5 +1,7 @@
 package datatypes;
 
+import java.util.LinkedList;
+
 /**
  * 
  * @author TimNP
@@ -15,20 +17,17 @@ public class Formula {
 	 */
 	private Formula secondSuccessor;
 	/**
-	 * number for operations:
-	 * 0 means NOT
-	 * 1 means AND
-	 * 2 means OR
+	 * number for used constructor:
+	 * 1 stands for a variable
+	 * 2 stands for NOT
+	 * 3 stands for AND
+	 * 4 stands for OR
 	 */
-	private int operation;
+	private int constructor;
 	/**
 	 * variable number
 	 */
 	private int varNr;
-	/**
-	 * logical value
-	 */
-	private boolean value;
 	
 	
 	/**
@@ -39,6 +38,7 @@ public class Formula {
 	public Formula variable(int varNr) {
 		Formula variable = new Formula();
 		variable.varNr = varNr;
+		variable.constructor = 1;
 		return variable;
 	}
 	
@@ -51,7 +51,7 @@ public class Formula {
 	public Formula not() {
 		Formula not = new Formula();
 		not.firstSuccessor = this;
-		not.operation = 0;
+		not.constructor = 2;
 		return not;
 	}
 	
@@ -65,7 +65,7 @@ public class Formula {
 		Formula and = new Formula();
 		and.firstSuccessor = this;
 		and.secondSuccessor = secondSuccessor;
-		and.operation = 1;
+		and.constructor = 3;
 		return and;
 	}
 	
@@ -79,7 +79,90 @@ public class Formula {
 		Formula or = new Formula();
 		or.firstSuccessor = this;
 		or.secondSuccessor = secondSuccessor;
-		or.operation = 2;
+		or.constructor = 4;
 		return or;
+	}
+	
+	
+	/**
+	 * Evaluates the Formula under a given assignment.
+	 * @param assignedOne - list of all variables assigned one
+	 * @return the value of the Formula as a boolean
+	 */
+	public boolean assign(LinkedList<Integer> assignedOne) {
+		/**
+		 * a switch for the possible constructors for this Formula
+		 */
+		switch (constructor) {
+			/**
+			 * First case: The Formula represents a variable.
+			 */
+			case 1:
+				/**
+				 * If the variable is assigned the logical value one,
+				 * true is returned. 
+				 */
+				if(assignedOne.contains(this.varNr)) {
+					return true;
+				}
+				/**
+				 * Otherwise the variable is assigned the logical value zero
+				 * or it isn't assigned anything. Therefore false is returned. 
+				 */
+				return false;
+			/**
+			 * Second case: The Formula represents a logical negation.
+			 */
+			case 2:
+				/**
+				 * calculating the value of the successor
+				 */
+				boolean firstSucVal = this.firstSuccessor.assign(assignedOne);
+				/**
+				 * returning the negated value of the successor
+				 */
+				return !firstSucVal;
+			/**
+			 * Third case: The Formula represents a logical conjunction.
+			 */
+			case 3:
+				/**
+				 * calculating the value of the first successor
+				 */
+				firstSucVal = this.firstSuccessor.assign(assignedOne);
+				/**
+				 * calculating the value of the second successor
+				 */
+				boolean secondSucVal = this.secondSuccessor.assign(assignedOne);
+				/**
+				 * returning the conjunction of the two successor values
+				 */
+				return firstSucVal && secondSucVal;
+			/**
+			 * Fourth case: The Formula represents a logical disjunction.
+			 */
+			case 4:
+				/**
+				 * calculating the value of the first successor
+				 */
+				firstSucVal = this.firstSuccessor.assign(assignedOne);
+				/**
+				 * calculating the value of the second successor
+				 */
+				secondSucVal = this.secondSuccessor.assign(assignedOne);
+				/**
+				 * returning the disjunction of the two successor values
+				 */
+				return firstSucVal || secondSucVal;
+			/**
+			 * Default case: None of the constructors was used.
+			 */
+			default:
+				/**
+				 * tentative default value: false
+				 */
+				// TODO user message
+				return false;
+		}
 	}
 }
