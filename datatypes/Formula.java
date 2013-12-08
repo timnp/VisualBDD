143ac30d4ab6ae9aa451b0f76471811ad980b7e1
timnp <tinpe@gmx.de>
2013-12-09 00:44:58 +0100

    added method that provides an entire function table

--- datatypes/Formula.java
+++ datatypes/Formula.java
@@ -2,6 +2,8 @@ package datatypes;
 
 import java.util.LinkedList;
 
+import javax.swing.JTable;
+
 /**
  * 
  * @author TimNP
@@ -285,5 +287,150 @@ public class Formula {
 			vars.clear();
 			return vars;
 		}
+	}
+	
+	
+	public JTable entireTruthTable() {
+		/**
+		 * initializing the greatest and least numbers
+		 * among the Formula's variables
+		 */
+		int maxVarNo = -1;
+		int minVarNo = -1;
+		/**
+		 * checking for each of the Formula's variable numbers
+		 * whether it's the greatest or the least
+		 */
+		for (int i : this.vars()) {
+			if (i > maxVarNo || maxVarNo == -1) {
+				/**
+				 * if a number is greater than the current maximum or there is
+				 * no maximum, the number becomes the new maximum
+				 */
+				maxVarNo = i;
+			}
+			if (i < minVarNo || minVarNo == -1) {
+				/**
+				 * if a number is less than the current minimum or there is
+				 * no minimum, the number becomes the new minimum
+				 */
+				minVarNo = i;
+			}
+		}
+		/**
+		 * initializing a list for all variable numbers from minVarNo
+		 * to maxVarNo
+		 */
+		LinkedList<Integer> vars = new LinkedList<Integer>();
+		/**
+		 * adding the variable numbers to the list
+		 */
+		for (int i = minVarNo ; i <= maxVarNo ; i++) {
+			vars.add(i);
+		}
+		/**
+		 * returning the truth table for "all" variables
+		 */
+		return truthTable(vars);
+	}
+	
+	
+	private JTable truthTable(LinkedList<Integer> vars) {
+		/**
+		 * initializing the column name array
+		 */
+		String[] columnNames = new String[vars.size() + 1];
+		/**
+		 * making each of the Formula's variables a column name
+		 */
+		for (int i : vars) {
+			columnNames[i - vars.getFirst()] = "X"+i;
+		}
+		/**
+		 * making the function value the last column name
+		 */
+		columnNames[vars.size()] =
+				"f(X" + vars.getFirst() + ",...,X" + vars.getLast() + ")";
+		/**
+		 * initializing the data array
+		 */
+		Integer[][] data = new Integer[vars.size() + 1]
+						[(int) Math.pow(2, vars.size())];
+		/**
+		 * writing zeros and ones for the variable values into the data array
+		 * 
+		 * iterating over the columns
+		 */
+		for (int column = 0 ; column < vars.size() ; column++) {
+			/**
+			 * A row of zeros and then ones before the next zero
+			 * is considered a "run".
+			 */
+			for (int run = 0 ; run < Math.pow(2, column) ; run++) {
+				/**
+				 * A zero or one after (below) another instance of
+				 * the same number is considered a "repeat".
+				 */
+				for (int repeat = 0 ; repeat < Math.pow(2, vars.size() - column- 1); repeat++) {
+					/**
+					 * Each "run" first has the repeats of zeros. 
+					 */
+					data[column][(int) Math.pow(2, vars.size() - run) * run + repeat] = 0;
+					/**
+					 * After (below) the zeros there are the repeats of ones.
+					 */
+					data[column][(int) (Math.pow(2, vars.size() - run) * run +
+							Math.pow(2, vars.size() - run - 1)) + repeat] = 1;
+				}
+			}
+		}
+		/**
+		 * initializing the list for the assignments to calculate the values
+		 * for the last column
+		 */
+		LinkedList<Integer> assignedOne = new LinkedList<Integer>();
+		/**
+		 * calculating the values one row after another
+		 */
+		for (int row = 0 ; row < Math.pow(2, vars.size()) ; row++) {
+			/**
+			 * clearing the assignment list
+			 */
+			assignedOne.clear();
+			/**
+			 * adding each variable in which's column is a one (in this row)
+			 * to the assignment list
+			 */
+			for (int column = 0 ; column < vars.size() ; column++) {
+				if (data[column][row] == 1) {
+					/**
+					 * If there is a one in the column, the respective variable
+					 * is added to the assignment list.
+					 */
+					assignedOne.add(vars.get(column));
+				}
+			}
+			/**
+			 * function value of this Formula's function under the assignment
+			 * represented by the row
+			 */
+			boolean funVal = this.assign(assignedOne);
+			/**
+			 * transferring the boolean function value into one/zero and
+			 * writing it into the data array
+			 */
+			if (funVal) {
+				data[vars.size()][row] = 1;
+			}
+			else data[vars.size()][row] = 0;
+		}
+		/**
+		 * creating the function table as a concrete JTable
+		 */
+		JTable entireFunTab = new JTable(data, columnNames);
+		/**
+		 * returning the function table
+		 */
+		return entireFunTab;
 	}
 }
