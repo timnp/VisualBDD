d39af09ce113cc515c0cde93710f599817d469be
timnp <tinpe@gmx.de>
2013-12-15 22:13:59 +0100

    corrected setter for the VariableOrderings list

--- datatypes/VariableOrdering.java
+++ datatypes/VariableOrdering.java
@@ -27,8 +27,31 @@ public class VariableOrdering {
 	 * @param ordList
 	 */
 	public void setOrdList(LinkedList<Integer> ordList) {
-		this.ordList = ordList;
-	}
+		/**
+		 * initializing a "storage" for the moved variable
+		 */
+		int movedVar;
+		/**
+		 * moving variables until the input list is empty
+		 */
+		while (!ordList.isEmpty()) {
+			/**
+			 * moving the first variable first to keep the order
+			 */
+			movedVar = ordList.getFirst();
+			/**
+			 * TODO?
+			 * tentative solution for multiple appearances of the same integer:
+			 * removing all but the first appearance 
+			 */
+			while (ordList.lastIndexOf(movedVar) != -1) {
+				ordList.removeLastOccurrence(movedVar);
+			}
+			/**
+			 * adding the variable to this objects list
+			 */
+			this.ordList.add(movedVar);
+		}	}
 	
 	
 	/**
