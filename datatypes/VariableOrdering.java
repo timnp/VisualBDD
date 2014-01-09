cc4aa42c989bc8ab8bd45c00157ccb82cd7043e1
timnp <tinpe@gmx.de>
2014-01-09 13:44:22 +0100

    added a method that removes all variables before a given one in a VariableOrdering

--- datatypes/VariableOrdering.java
+++ datatypes/VariableOrdering.java
@@ -105,5 +105,25 @@ public class VariableOrdering {
 			return true;
 		} else return false;
 	}
+	
+	
+	/**
+	 * method that provides a VariableOrdering like this one, except that all
+	 * variables in the VariableOrdering before the given one are removed
+	 * @param var
+	 * @return the reduced VariableOrdering
+	 */
+	public VariableOrdering removeBefore(int var) {
+		// initializing the new VariableOrdering as this one
+		VariableOrdering reducedOrd = this;
+		// getting the index of the given variable
+		int index = reducedOrd.indexOf(var);
+		// removing all variables in the VariableOrdering before the given one
+		for (int i = 0; i < index; i++) {
+			reducedOrd.ordList.removeFirst();
+		}
+		// returning the reduced VariableOrdering
+		return reducedOrd;
+	}
 
 }
