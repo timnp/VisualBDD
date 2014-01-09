0f0ccadf1f4f0120ea99239578313348cc9235bb
timnp <tinpe@gmx.de>
2014-01-09 18:04:31 +0100

    moved the case for no given VariableOrdering to the comparator

--- datatypes/VariableOrderingComparator.java
+++ datatypes/VariableOrderingComparator.java
@@ -29,28 +29,44 @@ public class VariableOrderingComparator implements Comparator<Integer>{
 	 */
 	@Override
 	public int compare(Integer a, Integer b) {
-		// getting the indexes of the two variables in the VariableOrdering
-		int indexA = varOrd.indexOf(a);
-		int indexB = varOrd.indexOf(b);
-		// If the two indexes are the same, the two variables are equal 
-		// by means of the VariableOrdering.
-		if (indexA == indexB) {
-			return 0;
+		// If there is no given VariableOrdering, the variables are assumed to 
+		// be sorted ascending with the lowest number on the first/highest 
+		// position.
+		if (this.varOrd.getOrdList().isEmpty()) {
+			if (a == b) {
+				return 0;
+			} else {
+				return b - a;
+			}
 		}
-		// If the first variable isn't part of the VariableOrdering, the second 
-		// one is higher by means of the VariableOrdering.
-		else if (indexA < 0) {
-			return -1;
+		// If there is a given VariableOrdering, the variables can be compared 
+		// by its means.
+		else {
+			// getting the indexes of the two variables in the VariableOrdering
+			int indexA = varOrd.indexOf(a);
+			int indexB = varOrd.indexOf(b);
+			// If the two indexes are the same, the two variables are equal 
+			// by means of the VariableOrdering.
+			if (indexA == indexB) {
+				return 0;
+			}
+			// If the first variable isn't part of the VariableOrdering, the 
+			// second one is higher by means of the VariableOrdering.
+			else if (indexA < 0) {
+				return -1;
+			}
+			// If the second variable isn't part of the VariableOrdering, the 
+			// first one is higher by means of the VariableOrdering.
+			else if (indexB <0) {
+				return 1;
+			}
+			// Otherwise the two variables are both part of the 
+			// VariableOrdering and aren't equal by its means. A lower index 
+			// means a higher position in a VariableOrdering and vice versa.
+			else return indexB - indexA;
+
 		}
-		// If the second variable isn't part of the VariableOrdering, the first 
-		// one is higher by means of the VariableOrdering.
-		else if (indexB <0) {
-			return 1;
-		}
-		// Otherwise the two variables are both part of the VariableOrdering 
-		// and aren't equal by its means. A lower index means a higher position 
-		// in a VariableOrdering and vice versa.
-		else return indexB - indexA;
+		
 	}
 
 }
