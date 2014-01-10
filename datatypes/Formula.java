f8846fee45602f10c32dde02b98ed52b08470da2
timnp <tinpe@gmx.de>
2014-01-10 01:49:01 +0100

    finished toOBDD method for Formulas

--- datatypes/Formula.java
+++ datatypes/Formula.java
@@ -278,49 +278,209 @@ public class Formula {
 	}
 	
 	
-//	public OBDD toOBDD() {
-//		// a switch for the possible constructors
-//		switch (this.constructor) {
-//		// Zeroth case: The Formula represents a constant.
-//		case 0:
-//			// If this constant Formula is tautological, the OBDD has to be too.
-//			if (this.value) {
-//				// Because the Formula has no successors and doesn't represent
-//				// a variable itself, the set of its variables is empty.
-//				// So simply a row of redundant nodes leading to the 1-terminal 
-//				// is created (bottom up) with all variables of the 
-//				// VariableOrdering (empty or not). 
-//				// The first and lowest node is the 1-terminal itself.
-//				OBDD currentNode = OBDD.ONE;
-//				// Then for each variable in the VariableOrdering a 
-//				// redundant node is added. Since the "cons" method is 
-//				// constructive, the current node may be overwritten 
-//				// without losing any node constructed this way.
-//				for (int i = this.varOrd.size() - 1; i >= 0; i--) {
-//					currentNode = 
-//							currentNode.cons(this.varOrd.get(i), currentNode, this.varOrd);
-//				}
-//			}
-//			// Otherwise the Formula is contradictory and so the OBDD has to be.
-//			else {
-//				// In this case also simply a row of redundant nodes leading to 
-//				// the 1-terminal is created (bottom up) with all variables of 
-//				// the VariableOrdering (empty or not). 
-//				// The first and lowest node is the 1-terminal itself.
-//				OBDD currentNode = OBDD.ONE;
-//				// Then for each variable in the VariableOrdering a 
-//				// redundant node is added. Since the "cons" method is 
-//				// constructive, the current node may be overwritten 
-//				// without losing any node constructed this way.
-//				for (int i = this.varOrd.size() - 1; i >= 0; i--) {
-//					currentNode = 
-//							currentNode.cons(this.varOrd.get(i), currentNode, this.varOrd);
-//				}
-//			}
-//		// First case: The Formula represents a variable.
-//		case 1:
-//		}
-//	}
+	/**
+	 * method that provides 
+	 * @return
+	 */
+	public OBDD toOBDD() {
+		// the number of variables in the VariableOrdering
+		int varOrdSize = this.varOrd.size();
+		// a switch for the possible constructors
+		switch (this.constructor) {
+		// Zeroth case: The Formula represents a constant.
+		case 0:
+			// If this constant Formula is tautological, the OBDD has to be too.
+			if (this.value) {
+				// Because the Formula has no successors and doesn't represent
+				// a variable itself, the set of its variables is empty.
+				// The node to begin with is the 1-terminal itself.
+				OBDD currentNode = OBDD.ONE;
+				// initializing a list for the OBDD nodes of the layer below 
+				// the current one
+				LinkedList<OBDD> layerBelow = new LinkedList<OBDD>();
+				// If the VariableOrdering isn't empty, more nodes are 
+				// constructed.
+				if (varOrdSize > 0) {
+					// First the lowest (decision node) layer consisting of 
+					// redundant nodes leading to the 1-terminal is constructed.
+					for (int j = 0; j < Math.pow(2, varOrdSize - 1); j++) {
+						// Since the "cons" method is constructive, the current 
+						// node may be overwritten without losing any node 
+						// constructed this way.
+						currentNode = currentNode.cons(this.varOrd.getLast(), currentNode, this.varOrd);
+						// adding the nodes to the list of the layer below for 
+						// the layer above
+						layerBelow.add(currentNode);
+					}
+					// The rest of the layers is constructed the same way: Each 
+					// node gets two different nodes of the layer below as 
+					// children.
+					for (int i = this.varOrd.size() - 2; i >= 0; i--) {
+						// The number of nodes decreases each layer from bottom 
+						// to top by half.
+						for (int j = 0; j < Math.pow(2, i); j++) {
+							currentNode = 
+									layerBelow.poll().cons(this.varOrd.get(i), layerBelow.poll(), this.varOrd);
+							// adding the new nodes to the layer below list
+							layerBelow.add(currentNode);
+						}
+					}
+				}
+				// The constructed OBDD's root is returned.
+				return currentNode;
+			}
+			// Otherwise the Formula is contradictory and so the OBDD has to be.
+			else {
+				// In this case the node to begin with is the 0-terminal.
+				OBDD currentNode = OBDD.ZERO;
+				// initializing a list for the OBDD nodes of the layer below 
+				// the current one
+				LinkedList<OBDD> layerBelow = new LinkedList<OBDD>();
+				// If the VariableOrdering isn't empty, more nodes are 
+				// constructed.
+				if (varOrdSize > 0) {
+					// First the lowest (decision node) layer consisting of 
+					// redundant nodes leading to the 0-terminal is constructed.
+					for (int j = 0; j < Math.pow(2, varOrdSize - 1); j++) {
+						currentNode = currentNode.cons(this.varOrd.getLast(), currentNode, this.varOrd);
+						// adding the nodes to the list of the layer below for 
+						// the layer above
+						layerBelow.add(currentNode);
+					}
+					// The rest of the layers is constructed the same way: Each 
+					// node gets two different nodes of the layer below as 
+					// children.
+					for (int i = this.varOrd.size() - 2; i >= 0; i--) {
+						// The number of nodes decreases each layer from bottom 
+						// to top by half.
+						for (int j = 0; j < Math.pow(2, i); j++) {
+							currentNode = 
+									layerBelow.poll().cons(this.varOrd.get(i), layerBelow.poll(), this.varOrd);
+							// adding the new nodes to the layer below list
+							layerBelow.add(currentNode);
+						}
+					}
+				}
+				// The constructed OBDD's root is returned.
+				return currentNode;
+			}
+		// First case: The Formula represents a variable.
+		case 1:
+			// getting the index of the specified variable in the 
+			// VariableOrdering
+			int varIndex = this.varOrd.get(this.varNr);
+			// If the specified variable is part of the VariableOrdering, an
+			// OBDD with all of the VariableOrdering's variables can be 
+			// constructed.
+			if (varIndex >= 0) {
+				// initializing a variable for the nodes that are being 
+				// constructed
+				OBDD currentNode = OBDD.ONE;
+				// initializing a list for the OBDD nodes of the layer below 
+				// the current one
+				LinkedList<OBDD> layerBelow = new LinkedList<OBDD>();
+				if (varIndex == varOrdSize - 1) {
+					// If the specified variable is the last in the 
+					// VariableOrdering, the lowest layer consists of OBDD 
+					// nodes that decide, whether the value is 1 or 0. 
+					for (int j = 0; j < Math.pow(2, varIndex); j++) {
+						currentNode = 
+								OBDD.ONE.cons(this.varNr, OBDD.ZERO, this.varOrd);
+						layerBelow.add(currentNode);
+					}
+				} else {
+					// Otherwise there are two groups of nodes in the layers 
+					// below the layer of the specified variable: the "zero" 
+					// nodes, which lead only to the 0-terminal, and the 
+					// 1-nodes, which lead only to the 1-terminal.
+					// The lowest non-terminal layer has to be constructed 
+					// separately because of the "different" number of nodes in 
+					// the layer below.
+					// initializing two lists for the "zero" nodes and the 
+					// "one" nodes of the layer below
+					LinkedList<OBDD> layerBelowZero = new LinkedList<OBDD>();
+					LinkedList<OBDD> layerBelowOne = new LinkedList<OBDD>();
+					for (int j = 0; j < Math.pow(2, varOrdSize - 2); j++) {
+						// constructing the lowest "zero" nodes
+						currentNode = 
+								OBDD.ZERO.cons(this.varOrd.getLast(), OBDD.ZERO, this.varOrd);
+						// adding the "zero" nodes to their list
+						layerBelowZero.add(currentNode);
+						// constructing the lowest "one" nodes
+						currentNode = 
+								OBDD.ONE.cons(this.varOrd.getLast(), OBDD.ONE, this.varOrd);
+						// adding the "one" nodes to their list
+						layerBelowOne.add(currentNode);
+					}
+					// constructing the other layers below the layer of the 
+					// specified variable
+					for (int i = this.varOrd.size() - 2; i > varIndex; i--) {
+						for (int j = 0; j < Math.pow(2, i - 1); j++) {
+							// constructing the "zero" nodes
+							currentNode = 
+									layerBelowZero.poll().cons(this.varOrd.get(i), layerBelowZero.poll(), this.varOrd);
+							// adding the "zero" nodes to their list
+							layerBelowZero.add(currentNode);
+							// constructing the "one" nodes
+							currentNode = 
+									layerBelowOne.poll().cons(this.varOrd.get(i), layerBelowOne.poll(), this.varOrd);
+							// adding the "one" nodes to their list
+							layerBelowOne.add(currentNode);
+						}
+					}
+					// constructing the layer of the specified variable
+					for (int j = 0; j < Math.pow(2, varIndex); j++) {
+						currentNode = 
+								layerBelowOne.poll().cons(this.varNr, layerBelowZero.poll(), this.varOrd);
+						// adding the nodes to the list for the layers above
+						layerBelow.add(currentNode);
+					}
+				}
+				// Above the specified variable's layer each node has any two 
+				// nodes from the layer below as its children.
+				for (int i = varIndex - 1; i >= 0; i--) {
+					for (int j = 0; j < Math.pow(2, i); j++) {
+						currentNode = 
+								layerBelow.poll().cons(this.varOrd.get(i), layerBelow.poll(), this.varOrd);
+						// adding the nodes to the layer below list
+						layerBelow.add(currentNode);
+					}
+				}
+				// The constructed OBDD's root is returned.
+				return currentNode;
+			}
+			// If the specified variable isn't part of the VariableOrdering 
+			// (possibly because it's empty), just one node is constructed.
+			else return OBDD.ONE.cons(this.varNr, OBDD.ZERO, this.varOrd);
+		// Second case: The Formula represents a logical negation.
+		case 2:
+			// First the OBDD for the successor is constructed.
+			OBDD successorOBDD = this.firstSuccessor.toOBDD();
+			// returning the negated successor OBDD
+			return successorOBDD.negate();
+		// Third case: The Formula represents a logical conjunction.
+		case 3:
+			// First the OBDDs for the two successors are constructed.
+			OBDD firstSuccessorOBDD = this.firstSuccessor.toOBDD();
+			OBDD secondSuccessorOBDD = this.secondSuccessor.toOBDD();
+			// applying the logical conjunction "and" on the two successor 
+			// OBDDs and returning the result;
+			return firstSuccessorOBDD.apply(secondSuccessorOBDD, OBDD.AND, this.varOrd);
+		// Fourth case: The Formula represents a logical disjunction.
+		case 4:
+			// First the OBDDs for the two successors are constructed.
+			firstSuccessorOBDD = this.firstSuccessor.toOBDD();
+			secondSuccessorOBDD = this.secondSuccessor.toOBDD();
+			// applying the logical disjunction "or" on the two successor OBDDs 
+			// and returning the result;
+			return firstSuccessorOBDD.apply(secondSuccessorOBDD, OBDD.OR, this.varOrd);
+		// Default case: None of the given constructors was used.
+		default:
+			// tentative value: null
+			// TODO user message
+			return null;
+		}
+	}
 	
 	
 	/**
