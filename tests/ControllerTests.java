6615d42cf77ffc83ed90d06862aa186a4095d390
timnp <tinpe@gmx.de>
2014-02-02 23:00:02 +0100

    moved the String to Formula test from the experimental class to the controller test class

--- tests/ControllerTests.java
+++ tests/ControllerTests.java
@@ -5,9 +5,17 @@ import controller.*;
 
 public class ControllerTests {
 	public static void main(String[] args) {
-		String input = " -X3  *x1  +-(X2   + X1*x4  * x3) ";
-		Formula f = FormulaController.stringToFormula(input);
-		String output = f.toString();
-		System.out.println(output);
+		// a "messy" but possible input String for a Formula
+		String f_string = "x2 + x1 *  -(X2 * x1) * -x3";
+		// converting the String into a Formula String
+		String f_fstring = FormulaController.toFormulaString(f_string);
+		// creating a Formula from the Formula String
+		Formula f = new Formula(f_fstring);
+		// converting the Formula back into a Formula String
+		String f_fstring2 = f.toString();
+		// printing all three Strings representing the same Formula
+		System.out.println(f_string);
+		System.out.println(f_fstring);
+		System.out.println(f_fstring2);
 	}
 }
--- tests/ExperimentalTests.java
+++ tests/ExperimentalTests.java
@@ -7,11 +7,7 @@ import controller.FormulaController;
 public class ExperimentalTests {
 	
 	public static void main(String[] args) {
-		String f_string = "x2 + x1 *  -(X2 * x1) * -x3";
-		String f_fstring = FormulaController.toFormulaString(f_string);
-		Formula f = new Formula(f_fstring);
-		String f_fstring2 = f.toString();
-		System.out.println(f_fstring2);
+
 	}
 
 }
