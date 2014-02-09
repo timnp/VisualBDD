4fb19c9bcd66f7325755ebceb7843fc6c3ff40d7
timnp <tinpe@gmx.de>
2014-02-09 23:14:11 +0100

    added a HashMap for the OBDDs and their changes including positions of the nodes

--- controller/ObddController.java
+++ controller/ObddController.java
@@ -1,9 +1,38 @@
 package controller;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 import model.OBDD;
+import model.Pair;
 
 public class ObddController {
-	public static void obddToLayout(OBDD g) {
-		
+	/**
+	 * HashMap for storing a stack for each OBDD currently worked with
+	 */
+	private static HashMap<
+		// the name of the OBDD
+		String,
+		// the stack for the OBDD
+		Stack<
+			// a pair of the OBDD and its nodes' positions
+			Pair<
+				// the OBDD itself (at that point)
+				OBDD,
+				// a HashMap for the position of each of the OBDD's nodes
+				HashMap<
+					// the node's ID
+					Integer,
+					// the node's (relative) position
+					Pair<Double, Double>>>>> obddStacks;
+	
+	private static void obddToLayout(String obddName, int horizontalPixels, 
+			int verticalPixels) {
+		 if (obddStacks.containsKey(obddName)) {
+			 
+		 }
+		 else {
+			 //TODO user message
+		 }
 	}
 }
