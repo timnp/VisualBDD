175b545c6d9475682e5f74bdc3c107a13bbba36e
timnp <tinpe@gmx.de>
2014-02-13 13:07:05 +0100

    added the positioning of the terminals to the abstract layout

--- model/AbstractObddLayout.java
+++ model/AbstractObddLayout.java
@@ -28,26 +28,28 @@ public class AbstractObddLayout {
 	public AbstractObddLayout(OBDD obdd) {
 		// retrieving the OBDD's layers
 		obddLayers = obdd.getLayers();
-		// the OBDD's number of layers
-		int numberOfLayers = obddLayers.size();
+		// the OBDD's number of layers (including the terminal layer)
+		int numberOfLayers = obddLayers.size() + 1;
 		// initializing a the current node's "position" 
 		// (its layer and its position in that layer) 
 		int currentLayerNumber = 0;
 		int currentLayerPosition = 0;
+		// variable for the nodes' vertical position
+		double verticalPosition;
 		// collecting the OBDD nodes from each layer
 		for (int var : obddLayers.keySet()) {
 			// the current layer
 			LinkedList<OBDD> currentLayer = obddLayers.get(var);
+			// the current layer's nodes' vertical position
+			verticalPosition = 
+					(1.5 + 3 * currentLayerNumber) / (3 * numberOfLayers);
 			// iterator for the current layer list
 			java.util.Iterator<OBDD> iter = currentLayer.iterator();
 			// iterating over the layer list
 			while (iter.hasNext()) {
-				// the current node's vertical position
-				double verticalPosition = 
-						(1 + 3 * currentLayerNumber) / numberOfLayers;
 				// the current node's horizontal position
-				double horizontalPosition = 
-						(1 + 3 * currentLayerPosition) / currentLayer.size();
+				double horizontalPosition = (1.5 + 3 * currentLayerPosition) / 
+						(3 * currentLayer.size());
 				// storing the node's position in the position HashMap
 				positionMap.put(iter.next().getId(), new Pair<Double, Double>
 						(horizontalPosition, verticalPosition));
@@ -59,6 +61,11 @@ public class AbstractObddLayout {
 			currentLayerNumber++;
 			currentLayerPosition = 0;
 		}
+		// the terminals' vertical position
+		verticalPosition = 1 - 1 / (3 * numberOfLayers);
+		// defining the terminals' positions
+		positionMap.put(1, new Pair<Double, Double>(0.25, verticalPosition));
+		positionMap.put(0, new Pair<Double, Double>(0.75, verticalPosition));
 		// One third of the visual OBDD's height is divided to the nodes.
 		nodeSizeToHeight = ((1 / 3) / numberOfLayers);
 	}
